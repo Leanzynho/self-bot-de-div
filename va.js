ab4caf5723e9f6bc58558a7c98231172a28fefd9
Leanzynho <80873782+Leanzynho@users.noreply.github.com>
2021-03-18 02:10:42 -0300

    Add files via upload

---
+++ va.js
@@ -0,0 +1,9 @@
+const nome = "vagui1nho"
+
+var viado = true
+
+const ida = 15
+if(viado == true) viado = "ele é viado!";
+if(nome != "vaguinho") return console.log("O nome inserido tem que ser igual do nome dado ao da váriavel!")
+//if(ida != 14) ida = "Idade dele é de 14 anos!"
+console.log(`${nome} ${viado} ${ida} anos!`)
